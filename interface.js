0fc2f2acf8e389daceb8594dd016ab506add1c5b
Rabee Mghrabi <learningprogramming2019@gmail.com>
2020-07-21 12:42:30 +0100

    added API of weather

--- interface.js
+++ interface.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
+  $('#current-city').change(function() {
+  var city = $('#current-city').val();
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+  $('#current-temperature').text(data.main.temp);
+})
+})
   updateTemperature();
+  // it already has an opening and closing brackts so no need to add to the next line
 
   $('#up').click(function() {
     thermostat.up();
